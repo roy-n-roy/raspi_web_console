0694fba16ebe0a630da3f4facaa08dd91581604a
Roy <royroy.euph@gmail.com>
2019-09-19 01:29:47 +0900

    Delete files.

--- systemd/networkd-dispatcher/no-carrier.d/start_ap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-if [ $IFACE = "eth0" -o $IFACE = "usb0" ]; then
-	if ip link show eth0 | grep "state DOWN" > /dev/null && !(arp -ni usb0 | grep "ether" > /dev/null); then
-		date > /tmp/start_ap.txt;
-		systemctl start hostapd;
-	fi
-fi
--- systemd/networkd-dispatcher/routable.d/stop_ap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-if [ $IFACE = "eth0" -o $IFACE = "usb0" ]; then
-	if ip link show eth0 | grep "state UP" > /dev/null || arp -ni usb0 | grep "ether" > /dev/null; then
-		date > /tmp/stop_ap.txt;
-		systemctl stop hostapd;
-	fi
-fi
